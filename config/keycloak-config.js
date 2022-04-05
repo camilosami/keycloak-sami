56175529e52c4a705206135571b5bad98dfaa386
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-05 16:03:21 -0300

    npm init

---
+++ config/keycloak-config.js
@@ -0,0 +1,28 @@
+var session = require('express-session');
+var Keycloak = require('keycloak-connect');
+
+let _keycloak;
+
+function initKeycloak(memoryStore, keycloakConfig) {
+    if (_keycloak) {
+        console.warn("Trying to init Keycloak again!");
+        return _keycloak;
+    } 
+    else {
+        console.log("Initializing Keycloak...");
+        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+        return _keycloak;
+    }
+}
+
+function getKeycloak() {
+    if (!_keycloak){
+        console.error('Keycloak has not been initialized. Please called init first.');
+    } 
+    return _keycloak;
+}
+
+module.exports = {
+    initKeycloak,
+    getKeycloak
+};
